8a9040b12e2f5f9e36d5769c34e0034c30937d6a
Stacey Smich <ssmich@Staceys-MacBook-Pro.local>
2019-07-06 20:56:07 -0400

    Commit 6 - The Basics

--- js/app.js
+++ js/app.js
@@ -150,3 +150,65 @@ if (age < 16){
 } else {
 	console.log("Here are the keys!");
 }
+
+// II. Loops
+//	A. The basics
+//	Write a loop that will print out all the numbers from 0 to 10, 
+//	inclusive.
+
+for(let i = 0; i < 11; i++){
+	console.log(i);
+}
+//	Write a loop that will print out all the numbers from 10 up to 
+//	and including 400.
+
+for(let i = 10; i <= 400; i++){
+	console.log(i);
+}
+
+//	Write a loop that will print out every third number starting 
+//	with 11 and going no higher than 4000.
+
+for(let i = 11; i <= 4000; i+=3){
+	console.log(i);
+}
+
+//	🔴 Commit.
+
+//	B. Get even
+//	Print out the numbers that are within the range of 1 - 100.
+
+//	Adjust your code to add a message next to even numbers only that 
+//	says: "<-- is an even number".
+
+//	🔴 Commit.
+
+//	C. Give me Five
+//	For the numbers 0 - 100, print out "I found a number. High five!" 
+//	if the number is a multiple of five.
+//	Example Output:
+//	I found a 5. High five!
+//	I found a 10. High five!
+//	Add to the code from above to print out "I found a number. Three is 
+//	a crowd" if the number is a multiple of three.
+//	Example Output:
+// 	I found a 3. Three is a crowd
+// 	I found a 5. High five!
+// 	I found a 6. Three is a crowd
+// 	I found a 9. Three is a crowd
+// 	I found a 10. High five!
+// 	For numbers divisible by both three and five, be sure your code 
+//	prints both messages.
+// 	🔴 Commit.
+
+// D. Savings account
+// 	Write code that will save the sum of all the numbers between 1 - 10 
+//	to a variable called bank_account.
+// 	Check your work! Your banck_account should have $55 in it.
+
+// 	You got a bonus! Your pay is now doubled each week. Write code that 
+//	will save the sum of all the numbers between 1 - 100 multiplied by 2.
+// 	Check your work! Your banck_account should have $10,100 in it.
+
+// 	🔴 Commit.
+
