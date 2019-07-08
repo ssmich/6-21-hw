e17c22440d4021a198251b756ab14cdea41407b9
Stacey Smich <ssmich@Staceys-MacBook-Pro.local>
2019-07-08 12:18:39 -0600

    Commit 31 - Shopaholic and Object within object

--- js/app.js
+++ js/app.js
@@ -785,7 +785,7 @@ console.log(user);
 // 	Our user has changed his or her email address. Without changing the 
 //	original user object, update the email value to a new email address.
 
-user.email = "somegirl@gwiz.com";
+user.email = "someSUPERguy@gwiz.com";
 
 // 	Our user has had a birthday! Without changing the original user object, 
 //	increment the age value using the postfix operator. Hint: age++
@@ -802,32 +802,73 @@ console.log(user);
 user.location = "Somewhere, USA";
 console.log(user);
 
-// D. Shopaholic!
-// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchased array.
-// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchased array.
-// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchased array.
-// Console.log just the "Merino jodhpurs" from the purchased array.
-// 🔴 Commit.
+// 	D. Shopaholic!
+// 	Our user has purchased an item! They have purchased some 
+//	"carbohydrates". Using .push(), add the string "carbohydrates" 
+//	to the purchased array.
 
-// E. Object-within-object
-// Remember that you can add an object to an existing object in the same way that you can add any new property/value pair.
+user.purchased.push("carbohydrates");
+console.log(user.purchased);
 
-// If we want to give our user a friend with a name and age, we could write:
+// 	Our user has purchased an item! They have purchased some "peace of 
+//	mind". Using .push(), add the string "peace of mind" to the purchased 
+//	array.
 
-// user.friend = {
+user.purchased.push("peace of mind");
+console.log(user.purchased);
+
+// 	Our user has purchased an item! They have purchased some "Merino 
+//	jodhpurs". Using .push(), add the string "Merino jodhpurs" to the 
+//	purchased array. Console.log just the "Merino jodhpurs" from the purchased array.
+
+user.purchased.push("Merino jodhpurs");
+console.log(user.purchased[2]);
+
+// 	E. Object-within-object
+// 	Remember that you can add an object to an existing object in the same 
+//	way that you can add any new property/value pair. If we want to give 
+//	our user a friend with a name and age, we could write:
+// 	user.friend = {
 //     name: "Grace Hopper",
 //     age: 85
-// }
-// When we console.log user, we would see the friend object added to our user object.
+// 	}
+// 	When we console.log user, we would see the friend object added to our 
+//	user object. Write a friend object into your user object and give the 
+//	friend a name, age, location, and purchased array (empty for now).
 
-// Write a friend object into your user object and give the friend a name, age, location, and purchased array (empty for now)
-// Console.log just the friend's name
-// Console.log just the friend's location
-// CHANGE the friend's age to 55
-// The friend has purchased "The One Ring". Use .push() to add "The One Ring" to the friend's purchased array.
-// The friend has purchased "A latte". Use .push() to add "A latte" to the friend's purchased array.
-// Console.log just "A latte" from the friend's purchased array.
-// 🔴 Commit.
+user.friend = {
+	name: "Dudette",
+	age: 35,
+	location: "Somewhere Else, USA",
+	purchased: []
+}
+console.log(user.friend);
+
+// 	Console.log just the friend's name
+
+console.log(user.friend.name);
+
+// 	Console.log just the friend's location
+
+console.log(user.friend.location);
+
+// 	CHANGE the friend's age to 55
+
+user.friend.age = 55;
+console.log(user.friend.age);
+
+// 	The friend has purchased "The One Ring". Use .push() to add "The One 
+//	Ring" to the friend's purchased array.
+
+user.friend.purchased.push("The One Ring");
+console.log(user.friend.purchased);
+
+//  The friend has purchased "A latte". Use .push() to add "A latte" to 
+//  the friend's purchased array. Console.log just "A latte" from the 
+//	friend's purchased array.
+
+user.friend.purchased.push("A latte");
+console.log(user.friend.purchased[1]);
 
 // F. Loops
 // Write a for loop that iterates over the User's purchased array (NOT the friend's purchased array), and prints each element to the console.
